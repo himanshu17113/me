54db3bb91f85d43be883be6fef85c0341957fc61
Himanshu Singhal <himanshu17113@gmail.com>
2025-05-10 14:57:53 +0530

    Add CMake configuration and update footer visibility logic
    - Added CMake build files and configuration for Android x86_64 architecture.
    - Introduced metadata generation command and prefab configuration.
    - Updated footer widget to manage background color visibility based on footer visibility.
    - Refactored footer structure into UpperStripes and LowerStripes for better organization.
    - Enabled GPU validation mode in Xcode scheme for improved debugging.

--- lib/presentaton/home/pages/fotter.dart
+++ lib/presentaton/home/pages/fotter.dart
@@ -3,11 +3,10 @@ import 'package:me/const.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
 final ValueNotifier<bool> isFooterVisiblex = ValueNotifier(false);
+final ValueNotifier<bool> bgColorx = ValueNotifier(false);
 
 class Footer extends StatelessWidget {
-  Footer({super.key});
-
-  final ValueNotifier<bool> bgColorx = ValueNotifier(false);
+  const Footer({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,87 +16,122 @@ class Footer extends StatelessWidget {
         onVisibilityChanged: (info) {
           if (info.visibleFraction >= 0.5) {
             isFooterVisiblex.value = true;
+            // bgColorx.value = true;
           } else {
+            bgColorx.value = false;
             isFooterVisiblex.value = false;
           }
         },
         key: const ValueKey("footer"),
-        child: SizedBox(
-          width: screenWidth,
-          child: Column(
-            spacing: 0,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AnimatedContainer(
-                alignment: Alignment.topRight,
-                margin: EdgeInsets.all(0),
-                color: Theme.of(context).colorScheme.primaryFixed,
-                duration: Durations.medium2,
-                width: isFooterVisible ? screenWidth : 10,
-                height: screenHeight * .15,
+        child: ValueListenableBuilder(
+          valueListenable: bgColorx,
+          builder: (context, bgColor, child) {
+            return Container(
+              color: bgColor && isFooterVisible ? Theme.of(context).colorScheme.primaryFixed : Colors.transparent,
+              width: screenWidth,
+              height: screenHeight,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  UpperStipes(),
+                  LowerStripes(),
+                ],
               ),
-              AnimatedContainer(
-                color: Theme.of(context).colorScheme.primaryFixed,
-                duration: Durations.medium4,
-                margin: EdgeInsets.all(0),
-                width: isFooterVisible ? screenWidth : 0,
-                height: screenHeight * .15,
-              ),
-              AnimatedContainer(
-                color: Theme.of(context).colorScheme.primaryFixed,
-                duration: Durations.long2,
-                margin: EdgeInsets.all(0),
-                width: isFooterVisible ? screenWidth : 0,
-                height: screenHeight * .15,
-              ),
-              AnimatedContainer(
-                color: Theme.of(context).colorScheme.primaryFixed,
-                duration: Durations.long4,
-                margin: EdgeInsets.all(0),
-                width: isFooterVisible ? screenWidth : 0,
-                height: screenHeight * .15,
-              ),
-              ValueListenableBuilder(
-                valueListenable: bgColorx,
-                builder: (context, bgColor, child) => ColoredBox(
-                  color: bgColor ? Theme.of(context).colorScheme.onPrimaryFixed : Colors.transparent,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      AnimatedContainer(
-                        color: Theme.of(context).colorScheme.onPrimaryFixed,
-                        duration: Durations.extralong2,
-                        margin: EdgeInsets.zero,
-                        padding: EdgeInsets.zero,
-                        width: isFooterVisible ? screenWidth : 0,
-                        height: screenHeight * .14,
-                      ),
-                      AnimatedContainer(
-                        color: Theme.of(context).colorScheme.onPrimaryFixed,
-                        duration: Durations.extralong3,
-                        margin: EdgeInsets.zero,
-                        padding: EdgeInsets.zero,
-                        width: isFooterVisible ? screenWidth : 0,
-                        height: screenHeight * .14,
-                      ),
-                      AnimatedContainer(
-                        onEnd: () => bgColorx.value = true,
-                        color: Theme.of(context).colorScheme.onPrimaryFixed,
-                        duration: Durations.extralong4,
-                        margin: EdgeInsets.zero,
-                        padding: EdgeInsets.zero,
-                        width: isFooterVisible ? screenWidth : 0,
-                        height: screenHeight * .14,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            );
+          },
         ),
       ),
+    );
+  }
+}
+
+class LowerStripes extends StatelessWidget {
+  const LowerStripes({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AnimatedContainer(
+          color: Theme.of(context).colorScheme.onPrimaryFixed,
+          duration: Durations.extralong2,
+          margin: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
+          width: isFooterVisiblex.value ? screenWidth : 0,
+          height: screenHeight * .14,
+        ),
+        AnimatedContainer(
+          color: Theme.of(context).colorScheme.onPrimaryFixed,
+          duration: Durations.extralong3,
+          margin: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
+          width: isFooterVisiblex.value ? screenWidth : 0,
+          height: screenHeight * .13,
+        ),
+        AnimatedContainer(
+          onEnd: () => bgColorx.value = true,
+          color: Theme.of(context).colorScheme.onPrimaryFixed,
+          duration: Durations.extralong4,
+          margin: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
+          width: isFooterVisiblex.value ? screenWidth : 0,
+          height: screenHeight * .13,
+        ),
+      ],
+    );
+  }
+}
+
+class UpperStipes extends StatelessWidget {
+  const UpperStipes({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AnimatedContainer(
+          alignment: Alignment.topRight,
+          color: Theme.of(context).colorScheme.primaryFixed,
+          margin: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
+          duration: Durations.medium2,
+          width: isFooterVisiblex.value ? screenWidth : 10,
+          height: screenHeight * .15,
+        ),
+        AnimatedContainer(
+          color: Theme.of(context).colorScheme.primaryFixed,
+          duration: Durations.medium4,
+          margin: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
+          width: isFooterVisiblex.value ? screenWidth : 0,
+          height: screenHeight * .15,
+        ),
+        AnimatedContainer(
+          color: Theme.of(context).colorScheme.primaryFixed,
+          duration: Durations.long2,
+          margin: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
+          width: isFooterVisiblex.value ? screenWidth : 0,
+          height: screenHeight * .15,
+        ),
+        AnimatedContainer(
+          color: Theme.of(context).colorScheme.primaryFixed,
+          duration: Durations.long4,
+          margin: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
+          width: isFooterVisiblex.value ? screenWidth : 0,
+          height: screenHeight * .15,
+        ),
+      ],
     );
   }
 }
